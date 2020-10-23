52d406f46b2ea554d279c16ac9695063e257fe9c
Vojtech Jindra <vjindr18@student.aau.dk>
2020-10-23 14:52:04 +0200

    remove router to tackle heroku routing problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './App.css';
 import style from './style.module.css';
 
@@ -24,14 +23,8 @@ export default class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className={ style.loadingHeader }>The Fucking Fund</h1>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/">
-                { this.state.loading ? <Loader /> : '' }
-                <Home loadingCallback={ this._handleLoading } />
-              </Route>
-            </Switch>
-          </BrowserRouter>
+          { this.state.loading ? <Loader /> : '' }
+          <Home loadingCallback={ this._handleLoading } />
         </header>
       </div>
     );
